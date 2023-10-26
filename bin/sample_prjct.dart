843cd529f8958191d9f7a4e9efd63c32454997f7
Fathimathsafa <safahaneefa01@gmail.com>
2023-10-26 14:20:37 +0530

    Initial commit

---
+++ bin/sample_prjct.dart
@@ -0,0 +1,5 @@
+import 'package:sample_prjct/sample_prjct.dart' as sample_prjct;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sample_prjct.calculate()}!');
+}
---
+++ test/sample_prjct_test.dart
@@ -0,0 +1,8 @@
+import 'package:sample_prjct/sample_prjct.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
